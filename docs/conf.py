a3fcab16cf250064a7942b547a67d69671e30fbd
Anton de Winter <adewinter@dimagi.com>
2011-12-21 19:54:53 -0500

    More tweaking of the docs/conf.py file

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,8 @@ root = os.path.split(path_here)[0]
 up_root = os.path.split(root)[0]
 sys.path.append(up_root)
 sys.path.append(root)
+
+sys.path.insert(0, os.path.abspath('../'))
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
